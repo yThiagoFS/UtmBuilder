a0636ce789616f4a0e6785b8cb3860fd13528b66
Thiago Silva <thi.ferreira.silva03@gmail.com>
2023-07-08 22:30:44 -0300

    finished before unit tests

--- UtmBuilder.Core/Utm.cs
+++ UtmBuilder.Core/Utm.cs
@@ -1,5 +1,6 @@
 ﻿using UtmBuilder.Core.ValueObjects;
 using UtmBuilder.Core.Extensions;
+using UtmBuilder.Core.ValueObjects.Exceptions;
 
 namespace UtmBuilder.Core
 {
@@ -26,6 +27,33 @@ namespace UtmBuilder.Core
         public static implicit operator string(Utm utm)
             => utm.ToString();
 
+        public static implicit operator Utm(string link)
+        {
+            if(string.IsNullOrEmpty(link))
+                throw new InvalidUrlException();
+
+            var url = new Url(link);
+
+            var segments = url.Address.Split("?");
+
+            if(segments.Length == 1)
+                throw new InvalidUrlException("No segments were provided.");
+            
+
+            var pars = segments[1].Split("&");
+            var source = pars.Where(x => x.StartsWith("utm_source")).FirstOrDefault("").Split("=")[1];
+            var medium = pars.Where(x => x.StartsWith("utm_medium")).FirstOrDefault("").Split("=")[1];
+            var name = pars.Where(x => x.StartsWith("utm_campaign")).FirstOrDefault("").Split("=")[1];
+            var id = pars.Where(x => x.StartsWith("utm_id")).FirstOrDefault("").Split("=")[1];
+            var term = pars.Where(x => x.StartsWith("utm_term")).FirstOrDefault("").Split("=")[1];
+            var content = pars.Where(x => x.StartsWith("utm_content")).FirstOrDefault("").Split("=")[1];
+
+            var utm = new Utm(
+                new Url(segments[0]), 
+                new Campaign(source, medium, name, id, term, content));
+            return utm;
+        }
+
         public override string ToString()
         {
                 var segments = new List<string>();
@@ -39,5 +67,6 @@ namespace UtmBuilder.Core
 
                 return $"{Url.Address}?{string.Join("&", segments)}";
         }
+
     }
 }
